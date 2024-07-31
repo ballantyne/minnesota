ed4efb27b7d992066267539bbfbdeb7cf6a2567e
Scott Ballantyne <scottballantyne@protonmail.com>
2024-07-31 15:13:03 -0500

    Adding several other functions

--- bin/console.js
+++ bin/console.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const repl = require('node:repl');
 const portal = require(path.join(__dirname, '..', 'mblsportal.sos.state.mn.us'));
+const Entity = require(path.join(__dirname, '..', 'mblsportal.sos.state.mn.us', 'entity'));
+const fetch = require(path.join(__dirname, '..', 'mblsportal.sos.state.mn.us', 'fetch'));
 
 const r = repl.start('> ');
 
@@ -10,7 +12,19 @@ const r = repl.start('> ');
 Object.defineProperty(r.context, 'sos', {
   configurable: false,
   enumerable: true,
-  value: portal,
+  value: portal
+}); 
+
+Object.defineProperty(r.context, 'Entity', {
+  configurable: false,
+  enumerable: true,
+  value: Entity
+}); 
+
+Object.defineProperty(r.context, 'fetch', {
+  configurable: false,
+  enumerable: true,
+  value: fetch
 }); 
 
 
